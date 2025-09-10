29c4b201b6781f1912f204a58cdb8151a250f9be
Tejas Patel <tejasp1055@gmail.com>
2025-09-10 10:45:11 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use(bodyParser.json({limit:"30mb", extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
+app.use(cors());
+
+app.use('/posts', postRoutes)
+
+app.get('/', (req, res) =>{
+    res.send('Hello To Memories API !!!')
+});
+
+//mongoose connnection
+const CONNECTION_URL ='mongodb+srv://tejaspatel:*Tejaspatel1055@cluster0.vwxotuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const PORT = process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology: true})
+    .then(() =>app.listen(PORT,() => console.log(`Server runnning on port:-${PORT}`)))
+    .catch((err) => console.log(err.message) )
+
+
